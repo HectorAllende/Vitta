c67ffdfb7865d59ce969d882583e94c22c86d212
HecAllende <hectorallende@gmail.com>
2021-11-29 03:24:42 -0300

    Agregando logos

---
+++ js/slider.js
@@ -0,0 +1,58 @@
+export default class Slider{
+    constructor({elements, animationFunc, spee}){
+        this.elements = elements;
+        this.animationFunc =animationFunc
+
+        this.speed = speed
+
+        this.index = 0
+        this.size = elements.length;
+
+        this.innerNext = this.innerNext.bind(this)
+        this.stop = this.stop.bind(this)
+
+        // this.next = this.next.bind(this)
+        // this.prev = this.prev.bind(this)
+    }
+
+    innerNext(){
+        this.index++
+
+        if(this.index >= this.size) this.index = 0
+        this.animationFunc(this.elements[this.index])
+    }
+
+    innerNext(){
+        this.index--
+
+        if(this.index<0) this.index = this.size -1
+        this.animationFunc(this.elements[this.index])
+    }
+
+    next(){
+        this.innerNext();
+        if(this.interval){
+            this.stop()
+            this.play()
+        }
+    }
+
+    prev(){
+        this.innerPrev();
+        if(this.interval){
+            this.stop()
+            this.play()
+        }
+    }
+
+    play() {
+        this.interval = setInterval(() => {
+            this.innerNext()
+        }, this.speed);
+    }
+
+    stop() {
+        clearInterval(this.interval)
+    }
+
+}
---
+++ js/sliderDOM.js
@@ -0,0 +1,36 @@
+import Slider from './slider'
+
+let sliderImage = document.querySelector('#slider-image')
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    slider.play()
+})
+
+
+let slider = new Slider({
+    elements:[
+        {
+            image:'../imagenes/somos/fondo1.jpg'
+        },
+        {
+            image:'../imagenes/home/home.jpg'
+        }
+    ],
+
+    animationFunc: function(element){
+        imageContent.classList.add('hide')
+
+        setTimeout(()=>{
+            sliderImage.src = element.image
+
+            imageContent.classList.remove('hide')
+        }, 600)
+
+
+    },
+    speed: 5000
+
+
+})
+
+
