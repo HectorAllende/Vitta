d70a798e09c08af55adfc7b3aa434bd9dcaf376c
HecAllende <hectorallende@gmail.com>
2021-11-29 17:51:19 -0300

    controles finales

--- js/sliderDOM.js
+++ js/sliderDOM.js
@@ -2,35 +2,30 @@ import Slider from './slider'
 
 let sliderImage = document.querySelector('#slider-image')
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     slider.play()
 })
 
-
 let slider = new Slider({
-    elements:[
+    elements: [
         {
-            image:'../imagenes/somos/fondo1.jpg'
+            image: '../imagenes/somos/fondo1.jpg'
         },
         {
-            image:'../imagenes/home/home.jpg'
+            image: '../imagenes/home/home.jpg'
         }
     ],
 
-    animationFunc: function(element){
+    animationFunc: function (element) {
         imageContent.classList.add('hide')
 
-        setTimeout(()=>{
+        setTimeout(() => {
             sliderImage.src = element.image
 
             imageContent.classList.remove('hide')
         }, 600)
-
-
     },
     speed: 5000
-
-
 })
 
 
